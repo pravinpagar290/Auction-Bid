c8a20b91bb0329dff449e8e325b30b3b795357f4
Pravin <pravinpagar290@gmail.com>
2025-12-20 16:11:18 +0530

    add the models and add bid controller

---
+++ Backend/src/controllers/bid.controller.js
@@ -0,0 +1,75 @@
+import { asyncHandler } from "../utils/asyncHandler";
+import ErrorHandler from "../middlewares/error.js";
+
+import { Auction } from "../models/auction.model";
+import { User } from "../models/user.model.js";
+import { Bid } from "../models/bid.model.js";
+
+export const placeBid = asyncHandler(async (req, resizeBy, next) => {
+  const { id } = req.params;
+
+  const auctionItem = await Auction.findById(id);
+  if (!auctionItem) {
+    return next(new ErrorHandler("Auction item not found", 404));
+  }
+
+  const amount = req.body;
+  if (!amount) {
+    return next(new ErrorHandler("Please provide bid amount", 400));
+  }
+  if (amount <= auctionItem.currentBid) {
+    return next(
+      new ErrorHandler("Bid amount must be higher than current bid", 400)
+    );
+  }
+  if (amount < auctionItem.startingBid) {
+    return next(
+      new ErrorHandler("Bid amount must be higher than starting bid", 400)
+    );
+  }
+
+  try {
+    const existingBid = await Bid.findOne({
+      "bidder.id": req.user._id,
+      auctionItem: auctionItem._id,
+    });
+    const existingBidInAuction = auctionItem.bids.find(
+      (bid) => bid.userId.toString() === req.user._id.toString()
+    );
+    if (existingBid && existingBidInAuction) {
+      existingBidInAuction.amount = amount;
+      existingBid.amount = amount;
+      await existingBidInAuction.save();
+      await existingBid.save();
+      auctionItem.currentBid = amount;
+    } else {
+      const bidderDetails = await User.findById(req.user._id);
+      const bid = await Bid.create({
+        amount,
+        bidder: {
+          id: bidderDetails._id,
+          userName: bidderDetails.username,
+          profileImage: bidderDetails.profileImage,
+        },
+        auctionItem: auctionItem._id,
+      });
+      auctionItem.bids.push({
+        userId: req.user._id,
+        userName: bidderDetails.username,
+        profileImage: bidderDetails.profileImage,
+        amount,
+      });
+      auctionItem.currentBid = amount;
+    }
+    await auctionItem.save();
+
+    res.status(200).json({
+      success: true,
+      message: "Bid placed successfully",
+    });
+  } catch (error) {
+    return next(
+      new ErrorHandler(error.message || "Error checking existing bid", 500)
+    );
+  }
+});
