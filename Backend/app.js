923657313dcf98974e62d8fbab07a8d347b87704
Pravin <pravinpagar290@gmail.com>
2025-12-23 15:26:13 +0530

    Add auction and commission routes, and initialize cron jobs in app.js

--- Backend/app.js
+++ Backend/app.js
@@ -6,7 +6,12 @@ import fileUpload from "express-fileupload";
 import { connectionDB } from "./src/database/connection.db.js";
 import { errorMiddleware } from "./src/middlewares/error.middleware.js";
 import userRouter from "./src/router/user.router.js";
-
+import adminRouter from "./src/router/admin.router.js";
+import auctionRouter from "./src/router/auction.route.js";
+import bidRouter from "./src/router/bid.route.js";
+import commissionRouter from "./src/router/commission.route.js";
+import { verifyCommissionCron } from "./src/automation/verifyCommission.js";
+import { endedAuctionCron } from "./src/automation/endAuction.js";
 const app = express();
 config();
 
@@ -29,7 +34,13 @@ app.use(
 );
 
 app.use("/api/v1/users", userRouter);
+app.use("/api/v1/admin", adminRouter);
+app.use("/api/v1/auctions", auctionRouter);
+app.use("/api/v1/bids", bidRouter);
+app.use("/api/v1/commission", commissionRouter);
 
+verifyCommissionCron();
+endedAuctionCron();
 connectionDB();
 app.use(errorMiddleware);
 
