2147303626144ffd0f566a4125b0f5eea0fef6f7
Pravin <pravinpagar290@gmail.com>
2026-01-04 18:29:05 +0530

    add bidSlice

---
+++ Frontend/auction-bid/src/store/Slices/bidSlice.js
@@ -0,0 +1,36 @@
+import {createSlice} from "@reduxjs/toolkit"
+import axios from "axios"
+
+
+export const bidSlice = createSlice({
+    name:"bid",
+    initialState:{
+        loading:false
+    },
+    reducers:{
+        bidRequest(state,action){
+            state.loading=true
+        },
+        bidSuccess(state,action){
+            state.loading=false
+        },
+        bidFail(state,action){
+            state.loading=false
+        }
+    }
+})
+
+export const bid=(id,data)=>async(dispatch)=>{
+    dispatch(bidSlice.actions.bidRequest());
+    try {
+        const response= await axios.post(`http://localhost:5000/api/v1/bids/place_bid/${id}`,data,{
+            withCredentials:true,
+            headers: { "Content-Type": "application/json" },
+        });
+        dispatch(bidSlice.actions.bidSuccess())
+    } catch (error) {
+        dispatch(bidSlice.actions.bidFail())
+    }
+}
+
+export default bidSlice.reducer
